afa216c93ec82415d1cd214a5d1a91fa42fb0301
Alex <prelyadeveloper@gmail.com>
2018-03-23 14:23:17 +0200

    created main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductsListComponent } from './product-page/products-list/products-list.component';
+import { ProductComponent } from './product-page/product/product.component';
+import { FeedbackListComponent } from './product-page/product/feedback-list/feedback-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductPageComponent,
+    ProductsListComponent,
+    ProductComponent,
+    FeedbackListComponent
   ],
   imports: [
     BrowserModule
